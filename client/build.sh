45dd65683af5b37f4bf41e8299e64b49b3b0fd13
kamrulcoder <kamrulcoder@gmail.com>
2025-08-11 04:31:05 +0600

    last try to deploy

---
+++ client/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd client
+npm install --force
+npm run build
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
+
 import "./App.css";
 
 function App() {
